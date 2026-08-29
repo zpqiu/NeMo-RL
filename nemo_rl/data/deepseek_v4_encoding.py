00b7e6a79c1a2ac60a417dafaa79c8843e2d8443
Zhaopeng Qiu <alexq@nvidia.com>
2026-08-29 00:01:01 -0700

    chore: clarify vendored vLLM license
    Signed-off-by: Zhaopeng Qiu <alexq@nvidia.com>

--- nemo_rl/data/deepseek_v4_encoding.py
+++ nemo_rl/data/deepseek_v4_encoding.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+# Licensed under the Apache License, Version 2.0 (the "License");
 # ruff: noqa
 # fmt: off
 
